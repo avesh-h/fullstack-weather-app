4ead4d76db8e35a6885c625528817f47f9f40ca7
Avesh <aveshhasanfatta1155@gmail.com>
2023-05-02 17:06:12 +0530

    add env

--- server/app.js
+++ server/app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const router = require('./routes/index')
-
+const router = require("./routes/index");
+const dotenv = require("dotenv");
 
 app.use(express.json());
 
 app.use(cors());
 
-app.use('/',router)
+app.use("/", router);
 
-app.listen(3001, () => {
+dotenv.config();
+
+app.listen(process.env.PORT || 3001, () => {
   console.log("server is on");
 });
 
-
-
-//get weather data for current location 
+//get weather data for current location
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
 // get weather data for city name
